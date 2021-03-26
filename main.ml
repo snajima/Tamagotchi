baf0d96dc3b4414269d66e5cc26f2472bc2438b3
Your Name <jyz24@cornell.edu>
2021-03-26 18:04:34 -0400

    made avatar model

---
+++ main.ml
@@ -0,0 +1,36 @@
+open Yojson.Basic.Util
+
+type item = {
+  name : string;
+  cost : int;
+}
+
+type tamagotchi = {
+  breed : string;
+  mutable lifeStage : int;
+  mutable sleep : int;
+  mutable cleanliness : int;
+  mutable hunger : int;
+  mutable age : int;
+  mutable money : int;
+  mutable inventory : item list;
+}
+
+let item_of_json json =
+  {
+    name = json |> member "name" |> to_string;
+    cost = json |> member "cost" |> to_int;
+  }
+
+let from_json json =
+  {
+    breed = json |> member "breed" |> to_string;
+    lifeStage = json |> member "lifeStage" |> to_int;
+    sleep = json |> member "sleep" |> to_int;
+    cleanliness = json |> member "cleanliness" |> to_int;
+    hunger = json |> member "hunger" |> to_int;
+    age = json |> member "age" |> to_int;
+    money = json |> member "money" |> to_int;
+    inventory =
+      json |> member "inventory" |> to_list |> List.map item_of_json;
+  }
