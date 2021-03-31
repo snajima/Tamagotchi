f40218d7cbcce15b1c9efeb9c6bda0abf0368dd4
Your Name <jyz24@cornell.edu>
2021-03-31 14:07:58 -0400

    Added Death Exception

--- main.ml
+++ main.ml
@@ -18,6 +18,10 @@ type tamagotchi = {
 
 exception WrongLifeStage
 
+exception Death
+
+exception NegativeMoney
+
 let item_of_json json =
   {
     name = json |> member "name" |> to_string;
@@ -48,42 +52,48 @@ let increment_lifeStage tam =
   | "Baby" -> tam.lifeStage <- "Teenager"
   | "Teenager" -> tam.lifeStage <- "Adult"
   | "Adult" -> tam.lifeStage <- "Senior"
-  | "Senior" -> tam.lifeStage <- "Baby"
+  | "Senior" -> tam.lifeStage <- raise Death
   | _ -> raise WrongLifeStage
 
 let get_sleep tam = tam.sleep
 
 (** Amount can be positive or negative*)
 let set_sleep amount tam =
-  let old_amount = tam.sleep in
-  tam.sleep <- amount + old_amount
+  let new_amount = amount + tam.sleep in
+  if new_amount < 0 then raise Death else tam.sleep <- new_amount
 
 let get_cleanliness tam = tam.cleanliness
 
 (** Amount can be positive or negative*)
 let set_cleanliness amount tam =
-  let old_amount = tam.cleanliness in
-  tam.cleanliness <- amount + old_amount
+  let new_amount = amount + tam.cleanliness in
+  if new_amount < 0 then raise Death else tam.cleanliness <- new_amount
 
 let get_hunger tam = tam.hunger
 
 (** Amount can be positive or negative*)
 let set_hunger amount tam =
-  let old_amount = tam.hunger in
-  tam.hunger <- amount + old_amount
+  let new_amount = amount + tam.hunger in
+  if new_amount < 0 then raise Death else tam.hunger <- new_amount
 
 let get_age tam = tam.age
 
+(**The age ranges for lifestage is as follows: 0-5 for Baby, 6-10 for
+   Teenager, 11-25 for Adult, 26-35 for Senior. If incrementing the age
+   pushes the Tamagotchi to a new lifestage, then it automatically
+   changes its lifestage too *)
 let increment_age tam =
-  let old_age = tam.age in
-  tam.age <- 1 + old_age
+  let new_age = tam.age + 1 in
+  if new_age = 6 || new_age = 11 || new_age = 26 then
+    increment_lifeStage tam;
+  tam.age <- new_age
 
-let get_money tam = tam.money
-
-(** Amount can be positive or negative*)
+(** Amount can be positive or negative. Raise Exception NegativeMoney if
+    the new amount of money becomes negative.*)
 let set_money amount tam =
-  let old_amount = tam.money in
-  tam.money <- amount + old_amount
+  let new_amount = tam.money + amount in
+  if new_amount < 0 then raise NegativeMoney
+  else tam.money <- new_amount
 
 let get_inventory tam = tam.inventory
 
