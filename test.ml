eb954f1dde093cc486f3fe1bf239f349446fdbc8
Your Name <jyz24@cornell.edu>
2021-03-31 14:36:56 -0400

    created test file

---
+++ test.ml
@@ -0,0 +1,28 @@
+open OUnit2
+open Main
+
+let sample_baby = Yojson.Basic.from_file "baby.json"
+
+let babe = from_json sample_baby
+
+let sample_teen = Yojson.Basic.from_file "teen.json"
+
+let teen = from_json sample_teen
+
+let equal_value_test name a b = name >:: fun ctxt -> assert_equal a b
+
+let death_exc name fxn = name >:: fun ctxt -> assert_raises Death fxn
+
+let negative_money_exc name fxn =
+  name >:: fun ctxt -> assert_raises NegativeMoney fxn
+
+let wrong_lifestage_exc name fxn =
+  name >:: fun ctxt -> assert_raises WrongLifeStage fxn
+
+let main_tests = []
+
+let suite =
+  "test suite for Tamagotchi Final Project"
+  >::: List.flatten [ main_tests ]
+
+let _ = run_test_tt_main suite
