e1e537a962e78dc11b421f61162acc426beda301
JustinZ301 <justin10252010@gmail.com>
2021-05-22 18:15:15 -0400

    added happiness

--- state.ml
+++ state.ml
@@ -11,6 +11,7 @@ type tamagotchi = {
   mutable sleep : int;
   mutable cleanliness : int;
   mutable hunger : int;
+  mutable happy : int;
   mutable age : int;
   mutable money : int;
   mutable inventory : item list;
@@ -41,6 +42,7 @@ let init_tam file_name =
     sleep = json |> member "sleep" |> to_int;
     cleanliness = json |> member "cleanliness" |> to_int;
     hunger = json |> member "hunger" |> to_int;
+    happy = json |> member "happy" |> to_int;
     age = json |> member "age" |> to_int;
     money = json |> member "money" |> to_int;
     inventory =
@@ -56,7 +58,7 @@ let get_sleep tam = tam.sleep
 
 let set_sleep amount tam =
   let new_amount = amount + tam.sleep in
-  if new_amount < 0 then raise Death
+  if new_amount <= 0 then raise Death
   else if new_amount <= 100 then tam.sleep <- new_amount
   else tam.sleep <- 100;
   tam
@@ -65,7 +67,7 @@ let get_cleanliness tam = tam.cleanliness
 
 let set_cleanliness amount tam =
   let new_amount = amount + tam.cleanliness in
-  if new_amount < 0 then raise Death
+  if new_amount <= 0 then raise Death
   else if new_amount <= 100 then tam.cleanliness <- new_amount
   else tam.cleanliness <- 100;
   tam
@@ -74,9 +76,18 @@ let get_hunger tam = tam.hunger
 
 let set_hunger amount tam =
   let new_amount = amount + tam.hunger in
-  if new_amount < 0 then raise Death
+  if new_amount <= 0 then raise Death
   else if new_amount <= 100 then tam.hunger <- new_amount
   else tam.hunger <- 100;
+  tam
+
+let get_happy tam = tam.happy
+
+let set_happy amount tam =
+  let new_amount = amount + tam.happy in
+  if new_amount <= 0 then raise Death
+  else if new_amount <= 100 then tam.happy <- new_amount
+  else tam.happy <- 100;
   tam
 
 let get_age tam = tam.age
@@ -110,13 +121,18 @@ let increment_sleep tam = set_sleep increment tam
 
 let increment_cleanliness tam = set_cleanliness increment tam
 
+let increment_happy tam = set_happy increment tam
+
+let decrement_happy tam = set_happy (-increment) tam
+
 let step tam =
   tam.step <- tam.step + 1;
   if tam.step mod 365 = 0 then (
     ignore (increment_age tam);
     ignore (set_hunger (-10) tam);
     ignore (set_cleanliness (-10) tam);
-    ignore (set_sleep (-10) tam) );
+    ignore (set_sleep (-10) tam);
+    ignore (set_happy (-10) tam) );
   tam
 
 (**Saving a game idk i might work on this later*)
